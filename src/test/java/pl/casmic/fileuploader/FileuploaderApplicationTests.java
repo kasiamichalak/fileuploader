d0d0e342674570f89fd8c3087e54552bfedb0164
Katarzyna Michalak <casmic@o2.pl>
2021-04-16 13:01:27 +0200

    all untrucked files added

---
+++ src/test/java/pl/casmic/fileuploader/FileuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.casmic.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
